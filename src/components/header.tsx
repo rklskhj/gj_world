2dca6fe84502a4d5aee1e01a51d0a028450469ea
rklskhj <rklskhj2@naver.com>
2025-03-04 04:50:24 +0900

    👨‍🔧 build issue fix > transposition problem fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import DarkModeToggleButton from "@/components/darkModeToogle";
+import DarkModeToggleButton from "./darkModeToogle";
 
 export default function Header() {
   return (
